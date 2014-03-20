ea972ad962a1ae810c6a6d29fcd59ee200b99960
joeywillhite <joeycw328@gmail.com>
2014-03-20 16:26:05 -0700

    initial commit

---
+++ mapper.py
@@ -0,0 +1,5 @@
+def mapper():
+    for line in sys.stdin:
+        data=line.strip().split("\t")
+        date, time, city, product_category, cost, payment_method =data
+        print "{0}\t{1}".format(product_category, cost)
