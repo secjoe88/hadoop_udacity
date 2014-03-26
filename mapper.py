880d4b3ef993b8084794eb25adf9d71a1ebe5389
joeywillhite <joeycw328@gmail.com>
2014-03-26 16:52:56 -0700

    stdin support added

--- mapper.py
+++ mapper.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python2
+import sys
 def mapper():
     for line in sys.stdin:
-        data=line.strip().split("\t")
-        date, time, city, product_category, cost, payment_method =data
-        print("{0}\t{1}".format(product_category, cost))
+        data = line.strip().split('\t')
+        [date, time, city, product_category, cost, payment_method] = data
+        print("{0}\t{1}".format(payment_method, cost))
