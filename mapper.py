4f0b912d003647f48cbd904c283445affcc05816
joeywillhite <joeycw328@gmail.com>
2014-03-26 17:34:04 -0700

    added comment, changed mapper to sort by product category

--- mapper.py
+++ mapper.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python2
+
+## Mapper for mapreduce jobs on countrywide purchase data
 import sys
 for line in sys.stdin:
         data = line.strip().split('\t')
         [date, time, city, product_category, cost, payment_method] = data
-        print("{0}\t{1}".format(payment_method, cost))
+        print("{0}\t{1}".format(product_category, cost))
