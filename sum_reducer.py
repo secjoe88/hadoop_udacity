0a10c16f6e607dcad1331a2016b17eef631db023
joeywillhite <joeycw328@gmail.com>
2014-03-27 19:05:34 -0700

    added reducers for max value per key, and number of keys with value sum

---
+++ sum_reducer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python2
+##Reducer to sum the Value data for each Key
+import sys
+currentKey=""
+previousKey=""
+currentValue=0
+currentTotal=0
+
+for line in sys.stdin:
+    [currentKey, currentValue]=line.strip().split('\t')
+
+    if previousKey and (currentKey != previousKey):
+        print "{0}\t{1}".format(previousKey, currentTotal)
+        currentTotal=0
+
+
+    currentTotal += float(currentValue)
+    previousKey=currentKey
+
+
+print "{0}\t{1}".format(previousKey, currentTotal)
