f3eca49dfbc428b8956a962b4a85f3960add13af
joeywillhite <joeycw328@gmail.com>
2014-03-31 21:16:01 -0700

    optimized string mapper for regular expressions

--- project1/part2/string_mapper.py
+++ project1/part2/string_mapper.py
@@ -9,13 +9,17 @@ import re
 parser=argparse.ArgumentParser(usage='%(prog)s string', description='Prints Key/Value pair <string, 1>')
 parser.add_argument("string", help="String to match")
 
+##Mapper accepts exactly one parameter
 if len(sys.argv)!=2:
     parser.print_help()
     sys.exit()
 else:
-    argString=sys.argv[1]
+    pattern=re.compile(sys.argv[1])
 
-
+##Parse inputed lines using regex
 for line in sys.stdin:
-    if re.search(argString, line):
-        print "{0}\t1".format(argString)
+    ##check if line matches regex
+    match=pattern.search(line)
+    ##If so print the portion of the line that matches the regex
+    if match:
+        print "{0}\t1".format(match.string[match.start():match.end()])
