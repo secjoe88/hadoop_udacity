d2c7272f0e03a96b8176d702abee4b6c988c8409
joeywillhite <joeycw328@gmail.com>
2014-03-31 17:05:58 -0700

    based access mapper logic on regex

--- project1/access_mapper.py
+++ project1/access_mapper.py
@@ -4,16 +4,14 @@
 
 import sys
 import argparse
+import re
 
 parser=argparse.ArgumentParser(usage='%(prog)s [option1] [option2]', description='Returns the Key/Value pairs specified by [option1] [option2]')
 parser.add_argument("-ip", help="Return the  client's ip address as part of the Key/Value pair", action="store_true")
 parser.add_argument("-id", help="Return the client's identity as part of the Key/Value pair", action="store_true")
 parser.add_argument("-u", help="Return the client's username as part of the Key/Value pair", action="store_true")
 parser.add_argument("-t", help="Return the request's finish date/time as part of the Key/Value pair", action="store_true")
-parser.add_argument("-z", help="Return the clien't time zone as part of the Key/Value pair", action="store_true")
-parser.add_argument("-m", help="Return the request method as part of the Key/Value pair", action="store_true")
-parser.add_argument("-q", help="Return the request's query strings part of the Key/Value pair", action="store_true")
-parser.add_argument("-p", help="Return the request's protocol as part of the Key/Value pair", action="store_true")
+parser.add_argument("-q", help="Return the request's query string as part of the Key/Value pair", action="store_true")
 parser.add_argument("-c", help="Return the status code of the request as part of the Key/Value pair", action="store_true")
 parser.add_argument("-s", help="Return the requested object's size as part of the Key/Value pair", action="store_true")
 parser.add_argument("-1", help="Return the integer 1 as part of the Key/Value pair (must be second option)", action="store_true")
@@ -22,11 +20,21 @@ if len(sys.argv)!=3:
         parser.print_help()
         sys.exit()
 returnValues=['', '']
-for line in sys.stdin:
 
-        ## Read line from stdin and parse 
-        data = line.strip().split()
-        [ip, identity, username, time, zone, method, query, protocol, status, size] = data
+pattern=re.compile("( |\[.+\]|\".+\")")
+
+for line in sys.stdin:
+        data=[]
+
+        ## Read line from stdin and parse
+        
+        tmp=re.split(pattern, line.strip())
+        for l in tmp:
+                if l !="" and l!=" ":
+                        data.append(l)
+
+        
+        [ip, identity, username, time, query, status, size] = data
         
         ##Discern desired Key for Key/Value pair
         if sys.argv[1]=="-ip":
@@ -37,18 +45,12 @@ for line in sys.stdin:
                 returnValues[0]=username
         elif sys.argv[1]=="-t":
                 returnValues[0]=time
-        elif sys.argv[1]=="-z":
-                returnValues[0]=zone
-        elif sys.argv[1]=="-m":
-                returnValues[0]=method
         elif sys.argv[1]=="-q":
                 returnValues[0]=query
-        elif sys.argv[1]=="-p":
-                returnValues[0]=protocol
         elif sys.argv[1]=="-c":
                 returnValues[0]=status
         elif sys.argv[1]=="-s":
-            returnValues[0]=size
+                returnValues[0]=size
         else:
             parser.print_help()
             sys.exit()
@@ -63,14 +65,8 @@ for line in sys.stdin:
                 returnValues[1]=username
         elif sys.argv[2]=="-t":
                 returnValues[1]=time
-        elif sys.argv[2]=="-z":
-                returnValues[1]=zone
-        elif sys.argv[2]=="-m":
-                returnValues[1]=method
         elif sys.argv[2]=="-q":
                 returnValues[1]=query
-        elif sys.argv[2]=="-p":
-                returnValues[1]=protocol
         elif sys.argv[2]=="-c":
                 returnValues[1]=status
         elif sys.argv[2]=="-s":
@@ -80,3 +76,4 @@ for line in sys.stdin:
 
         ##Print Key/Vlue
         print("{0}\t{1}".format(returnValues[0], returnValues[1]))
+
