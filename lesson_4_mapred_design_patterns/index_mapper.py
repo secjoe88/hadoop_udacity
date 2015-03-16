73ce36bc091fb32d3992bacf742dc4d44440b0db
Jojo-PC <joeycw328@gmail.com>
2015-03-16 00:48:07 -0700

    created reducer, commented mapper

--- lesson_4_mapred_design_patterns/index_mapper.py
+++ lesson_4_mapred_design_patterns/index_mapper.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python2
 
 import sys
+import csv
+import re
 
-for line in sys.stdin:
-	in=line.split('\t')
-	print in	
+reader=csv.reader(sys.stdin, delimiter='\t', )
+writer=csv.writer(sys.stdout, delimiter='\t', quotechar='"', quoting=csv.QUOTE_ALL)
+pattern=re.compile('\W+')
+#skip first row, since it only defines column fields
+reader.next()
+
+for line in reader:
+    #store row id
+    current_id=line[0]
+    #split the body column into individual words
+    split_line=pattern.split(re.sub('<.+?>', ' ', line[4].replace('\n', ' ').strip()))
+    split_line=[word.lower() for word in split_line if word !='']
+    #print to stdout
+    for word in split_line: print "{0}\t{1}".format(word,current_id)
