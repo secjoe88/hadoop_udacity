73367d36909c5da6252ae64ef9c94965985cfb95
Josef Willhite <joeycw328@gmail.com>
2015-03-15 19:34:38 -0700

    file organization

--- project1/regurg_reducer.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python2
-##reducer to simply regurgitate inpute key/value pairs
-import sys
-key=""
-value=""
-for line in sys.stdin:
-	[key, value]=line.strip().split('\t')
-	print "{0}\t{1}".format(key, value)
