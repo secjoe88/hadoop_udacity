09174411bd6d75b7cb1265528ed59288bc88f681
Jojo-PC <joeycw328@gmail.com>
2015-03-16 22:59:47 -0700

    fixed reducer addition logic

--- lesson_4_mapred_design_patterns/mean_reducer.py
+++ lesson_4_mapred_design_patterns/mean_reducer.py
@@ -4,7 +4,7 @@ import csv
 import sys
 
 
-prevkey='';totalsales=0.;count=0
+prevkey=None;totalsales=0.;count=0
 weekdays={
     '0':'Monday',
     '1':'Tuesday',
@@ -19,8 +19,8 @@ reader=csv.reader(sys.stdin, delimiter='\t')
 for line in reader:
     #read in key value pair from stdin
     curkey, cursale=line;
-    cursale=int(cursale.replace('.',''));
-    prevkey=curkey if prevkey=='' else prevkey
+    cursale=float(cursale);
+    prevkey=curkey if prevkey==None else prevkey
     if curkey==prevkey:
         #if key on current line is same as key on previous line (they are same day of the week)
         #add cursale to the total for the current day of the week and increment the number of sales
@@ -29,8 +29,8 @@ for line in reader:
         count+=1
     else:
         #otherwise print mean sale for that weekday and reset totalsales and counter
-        average=str(int(totalsales/count))
-        print '%s\t%s.%s'%(weekdays[prevkey],average[0:-2],average[-2:])
+        
+        print '%s\t%.3f'%(weekdays[prevkey],totalsales/count)
         totalsales=cursale;
         count=1;
         
@@ -38,5 +38,4 @@ for line in reader:
     prevkey=curkey
         
 #print final solution
-average=str(int(totalsales/count))
-print '%s\t%s.%s'%(weekdays[prevkey],average[0:-2],average[-2:])
+print '%s\t%.3f'%(weekdays[prevkey],totalsales/count)
