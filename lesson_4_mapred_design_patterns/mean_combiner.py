2aa532d6402652c6b26be8a2a63623e457aa4b49
Jojo-PC <joeycw328@gmail.com>
2015-03-16 23:46:49 -0700

    added combiner script

---
+++ lesson_4_mapred_design_patterns/mean_combiner.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python2
+
+import csv
+import sys
+
+prevkey=None;totalsales=0.;count=0
+reader=csv.reader(sys.stdin, delimiter='\t')
+
+for line in reader:
+    #read in key value pair from stdin
+    curkey, cursale=line;
+    cursale=float(cursale);
+    prevkey=curkey if prevkey==None else prevkey
+    if curkey==prevkey:
+        #if key on current line is same as key on previous line (they are same day of the week)
+        #add cursale to the total for the current day of the week and increment the number of sales
+        #for that day of the week
+        totalsales+=cursale
+        count+=1
+    else:
+        #otherwise print mean sale for that weekday and reset totalsales and counter
+        
+        print '%s\t%f'%(curkey,totalsales/count)
+        totalsales=cursale;
+        count=1;
+        
+    
+    prevkey=curkey
+        
+#print final solution
+print '%s\t%f'%(curkey,totalsales/count)
+    
