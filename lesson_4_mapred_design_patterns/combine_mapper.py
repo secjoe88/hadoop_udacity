1d87892cc5a7ab0fc1e6d3650e50e1624ea25cd9
Jojo-PC <joeycw328@gmail.com>
2015-03-17 23:43:31 -0700

    altered to correctly handle multiple file input

--- lesson_4_mapred_design_patterns/combine_mapper.py
+++ lesson_4_mapred_design_patterns/combine_mapper.py
@@ -3,32 +3,24 @@ import csv
 import sys
 
 
-forum_node=open('/usr/hduser/input/forum_node.tsv', 'r')
-forum_users=open('/user/hduser/input/forum_users.tsv','r')
+reader=csv.reader(sys.stdin, delimiter='\t')
 
-node_reader=csv.reader(forum_node, delimiter='\t')
-users_reader=csv.reader(forum_users, delimiter='\t')
-node_reader.next();users_reader.next()
 
 writer=csv.writer(sys.stdout, delimiter='\t', quoting=csv.QUOTE_ALL)
 
-for line in node_reader:
-    #store desired data from forum post 
-    #with format key=[author_id], value=[id, title, tagnames, node_type, parentid, abs_parent_id, added_at, score]
-    key,value=[line[3]],[line[i] for i in [0,1,2,5,6,7,8,9]]
-    #print out user id of poster, node keyword identifier (b), and forum post info as value
-    #final format: 
-    #   [author_id, b, id, title, tagnames, node_type, parentid, abs_parent_id, added_at, score]
-    writer.writerow(key+['b']+value)
+for line in reader:
+    if len(line)==5: #user information line
+        #store user information
+        #with formatting key=[usr_ptr_id], value=[reputation, gold, silver, bronze]
+        key,value=[line[0]],line[1:]
+        #print user id, user keywork identifier (a), and remaining user information
+        writer.writerow(key+['a']+value)
+    else: #forum post information line
+        #store desired data from forum post 
+        #with format key=[author_id], value=[id, title, tagnames, node_type, parentid, abs_parent_id, added_at, score]
+        key,value=[line[3]],[line[i] for i in [0,1,2,5,6,7,8,9]]
+        #print out user id of poster, node keyword identifier (b), and forum post info as value
+        #final format: 
+        #   [author_id, b, id, title, tagnames, node_type, parentid, abs_parent_id, added_at, score]
+        writer.writerow(key+['b']+value)
 
-forum_node.close()
-
-
-for line in users_reader:
-    #store user information
-    #with formatting key=[usr_ptr_id], value=[reputation, gold, silver, bronze]
-    key,value=[line[0]],line[1:]
-    #print user id, user keywork identifier (a), and remaining user information
-    writer.writerow(key+['a']+value)
-    
-forum_users.close()
