b590419020b01122d0f1b69d582b7c13cbdbb73c
Jojo-PC <joeycw328@gmail.com>
2015-03-18 01:25:48 -0700

    added logic to handle fieldname line

--- lesson_4_mapred_design_patterns/combine_mapper.py
+++ lesson_4_mapred_design_patterns/combine_mapper.py
@@ -11,6 +11,7 @@ writer=csv.writer(sys.stdout, delimiter='\t', quoting=csv.QUOTE_ALL)
 
 for line in reader:
     if len(line)==5: #user information line
+        if line[0]=='user_ptr_id': continue
         #store user information
         #with formatting key=[usr_ptr_id], value=[reputation, gold, silver, bronze]
         key,value=[line[0]],line[1:]
@@ -18,6 +19,7 @@ for line in reader:
         writer.writerow(key+['a']+value)
     
     else: #forum post information line
+        if line[0]=='id': continue
         #store desired data from forum post 
         #with format key=[author_id], value=[id, title, tagnames, node_type, parentid, abs_parent_id, added_at, score]
         key,value=[line[3]],[line[i] for i in [0,1,2,5,6,7,8,9]]
